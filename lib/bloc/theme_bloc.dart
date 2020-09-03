34f89ae4db3b38aabb14e5b26a0a9b6e60cd7515
Pawid-prianto27 <pawitanto27@gmail.com>
2020-09-03 15:45:50 +0700

    loginpagefinish

--- lib/bloc/theme_bloc.dart
+++ lib/bloc/theme_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
 
 part 'theme_event.dart';
 part 'theme_state.dart';
